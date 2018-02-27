c311b9cdbcaa2205e9124ed374a74c9493ebd0c1
d4nyy <danyyil.luntovsky@gmail.com>
2018-02-27 14:36:50 +0100

    Moved CPU_USED_PERCENTAGES into the loop

--- rms.sh
+++ rms.sh
@@ -3,26 +3,27 @@
 DATE=$(date '+%d/%m/%Y %H:%M %Z')
 THRESHOLD=$1
 SUBJECT="$(hostname): CPU Consumption Alert => Utilization Exceeded Threshold."
-EMAIL=./emails/email.$$
+EMAIL=./email.$$
 MAIL_PROG=./emailer.sh
 PS_HEADER=$(ps aux | head -n 1)
 GET_PID=1
-CPU_USED_PERCENTAGES=($(ps aux | fgrep -v USER | sort -nr -k3 | head -10 \
-			       | tr -s " " | cut -d " " -f 3 | cut -d "." -f 1))
 
 while :
 do
+	sleep 10
+	CPU_USED_PERCENTAGES=($(ps aux | fgrep -v USER | sort -nr -k3 | head -10 \
+					| tr -s " " | cut -d " " -f 3 | cut -d "." -f 1))
+
 	for CPU_USED_PERCENTAGE in ${CPU_USED_PERCENTAGES[@]}
 	do
 		if [[ $CPU_USED_PERCENTAGE -gt $THRESHOLD && $GET_PID != ${PID_CHECK[*]} ]]; then
 
 			GET_PID=$(ps aux | fgrep "${CPU_USED_PERCENTAGE}." | fgrep -v grep | tr -s " " | cut -d " " -f 2)
 
-			echo $PS_HEADER >> $EMAIL
+			echo "$PS_HEADER" >> $EMAIL
 			echo '\n' >> $EMAIL
 			ps aux | fgrep "${CPU_USED_PERCENTAGE}." | fgrep -v grep >> $EMAIL
 			PID_CHECK+=("$GET_PID")
-			echo ${PID_CHECK[0]}
 			echo >> $EMAIL
 		fi
 	done
@@ -30,7 +31,7 @@ do
 	if [ -f $EMAIL ]; then
 		sed "1 i$DATE" $EMAIL > $EMAIL.tmp && mv $EMAIL.tmp $EMAIL
 		echo '\n' >> $EMAIL
-		$MAIL_PROG "$SUBJECT" "$EMAIL"
+		$MAIL_PROG "$SUBJECT" "$EMAIL" >/dev/null
 		rm -f $EMAIL
 	fi
 done
