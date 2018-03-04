23c42c759ce581791aab606caad224a46886dfd2
Dany Lunt <33714789+D4nyy@users.noreply.github.com>
2018-03-04 19:35:41 +0100

    Corrections

--- email_check.sh
+++ email_check.sh
@@ -3,9 +3,9 @@
 ###################################################################
 #Script Name	:       ResourceMonitoringScript
 #File Name	:	email_check.sh
-#Description	:	Dieses Script check ob man eine Nuene Email im Posteingang des angegebnen Google-Accounts hat.
+#Description	:	Dieses Script check ob man eine neue Email im Posteingang des angegebenen Google-Accounts hat.
 #			Es wird nach Name, Absender und ob der Prozess noch läuft gefiltert.
-#			Sollten alle Punkte zutreffen wird der über den Parameter übergebene Prozess gekillt
+#			Sollten alle Punkte zutreffen, wird der über den Parameter übergebene Prozess gekillt
 #Date           :       04.03.2018
 #Author       	:	Danyyil Luntovsky
 #Version       	:	1.0
@@ -19,12 +19,12 @@ VAR_PID="$1"
 ###SCRIPT###
 
 #-------------------------------------------------------[ Email listener ]-------------------------------------------------------#
-#Das ganze befinden sich in der unedlichen Schleife, die jede Minute ausgeführt wird
+#Das Ganze befindet sich in der uneidlichen Schleife, die jede Minute ausgeführt wird
 #VAR_CHECK_MESSAGE überprüft ob es eine Neue Email im Posteingang gibt die das Wort kill mit der dazugehörigen PID enthält
-#VAR_CHECK_SENDER überprüft ob die Email von dem gleichen Nutzer abgeschikt wurde (Ob es einen Antwort ist)
+#VAR_CHECK_SENDER überprüft ob die Email von dem gleichen Nutzer abgeschickt wurde (Ob es einen Antwort ist)
 #Danach wird mit einer if Abfrage geprüft ob der Prozess noch läuft, wenn nicht word das Skript beendet
 #In der nächsten if Abfrage wird die Nachricht und die PID geprüft
-#Und in der Letzten ob die nachricht auch wirklich von mir stammte
+#Und in der Letzten ob die Nachricht auch wirklich von mir stammte
 #Sollten die ganzen Abfragen true sein wird der Prozess gekillt und das Script geschlossen
 
 while true
@@ -34,8 +34,8 @@ do
 	VAR_CHECK_SENDER=$(curl -su modul122.info:gibbiX12345 https://mail.google.com/mail/feed/atom | awk -F "name" '{ print $2 }')	#Hole den Absender aus der neuen Email
 
 	if [ ! -z "$VAR_PID" ]; then		#Wenn der Prozess noch läuft
-		if [[ "$VAR_CHECK_MESSAGE_PID" = ">kill $VAR_PID" ]]; then	#Wenn die Nachricht kill ist und wenn die Prozess ID die selbe ist wie es dem Skript als Parameter übergeben wurde
-			if [ "$VAR_CHECK_SENDER" == ">me</" ]; then		#Wenn man selber der absender ist
+		if [[ "$VAR_CHECK_MESSAGE_PID" = ">kill $VAR_PID" ]]; then	#Wenn die Nachricht kill ist und wenn die Prozess ID dieselbe ist wie es dem Skript als Parameter übergeben wurde
+			if [ "$VAR_CHECK_SENDER" == ">me</" ]; then		#Wenn man selber der Absender ist
 				kill "$VAR_PID" > /dev/null			#Wird der Befehl kill mit dem Parameter [VAR_PID] aufgerufen
 				exit 0						#Das Skript wird beendet
 			fi
