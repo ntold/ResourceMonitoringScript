edf4571d3d01065756bc7d7722a2458b858e6cc3
Dany Lunt <33714789+D4nyy@users.noreply.github.com>
2018-03-04 19:18:13 +0100

    Added Quotes

--- emailer.sh
+++ emailer.sh
@@ -18,7 +18,7 @@ SERVER=smtp.googlemail.com:587
 USER=modul122.info
 PASS=gibbiX12345
 SUBJECT=$1
-MESSAGE=$(cat $2)
+MESSAGE=$(cat "$2")
 
 ###SCRIPT###
 sendEmail -f $FROM -t $TO -u "$SUBJECT" -m "$MESSAGE" -s $SERVER -xu $USER -xp $PASS
