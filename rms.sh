c830c4b162a68c3f71d097ee9d4ed9c70f3e7426
Dany Lunt <33714789+D4nyy@users.noreply.github.com>
2018-03-04 19:45:15 +0100

    Corrections

--- rms.sh
+++ rms.sh
@@ -15,9 +15,9 @@
 
 ###VARIABLES###
 DATE=$(date '+%d/%m/%Y %H:%M %Z')	#Das heutige Datum mit der Zeitzone
-THRESHOLD=$1				#Die Schwelle um die Email zu verschiken
+THRESHOLD=$1				#Die Schwelle um die Email zu verschicken
 SUBJECT="$(hostname): CPU Consumption Alert => Utilization Exceeded Threshold."	#Betreff der Email mit dem Hostnamen
-EMAIL=./email.$$			#Name der Emaildatei die erstellt wird, durch das '$$' (PID des Prozesses), wird der Name einzigartig
+EMAIL=./email.$$			#Name der Emaildatei, die erstellt wird, durch das '$$' (PID des Prozesses), wird der Name einzigartig
 MAIL_PROG=./emailer.sh			#Email Programm
 EMAIL_LISTENER=./email_check.sh		#Email listener Programm
 PS_HEADER="$(ps aux | head -n 1)"	#Die Kopfzeile der Email mit den Informationen wie Name, Befehl, PID usw.
@@ -32,11 +32,11 @@ do
         sleep 1		#Der untere Abschnitt des Codes wird jede x Sekunde(n) wiederholt
 
 	#-------------------------------------------------------[ Get PID and %CPU ]-------------------------------------------------------#
-	#In diedem Abschnitt werden die derzeitigen Prozente und deren Prozess ID ausgelesen
-	#Wenn mas das mit grep, sort & cut macht wird das ganze in einem String gespiechert in der Folgenden Reihenfolge: PID %CPU PID %CPU PID ... (jedes Paar 10 Mal)
+	#In diesem Abschnitt werden die derzeitigen Prozente und deren Prozess ID ausgelesen
+	#Wenn man das mit grep, sort & cut macht wird das Ganze in einem String gespeichert in der folgenden Reihenfolge: PID %CPU PID %CPU PID ... (jedes Paar 10 Mal)
 	#In der For-Schleife trenne ich diese Werte voneinander, die ungeraden Stellen im String (%CPU) gehen in ARR_PERCENTAGES
 	#Und die geraden (PID) in den ARR_PID
-	#Das geht so lange bis jede Stelle im String (STR_PID_PERCENTAGES) sortiert wurde
+	#Das geht so lange bis, jede Stelle im String (STR_PID_PERCENTAGES) sortiert wurde
 
         STR_PID_PERCENTAGES=$(ps aux | fgrep -v USER | sort -nr -k3 | head -10 | cut -c10-20 | cut -d "." -f 1)	#Mit dem Kommando ps aux werden die zwei Spalten PID und %CPU gegrept
 
@@ -55,10 +55,10 @@ do
 	#echo "${ARR_PERCENTAGES[*]}"
 	#exit 0
 	#-------------------------------------------------------[ Check & Send ]-------------------------------------------------------#
-	#Dies ist das Herzstück des Scripts und zwar geht es hier um die Überprüfung der ganzen Zahlen aus den beiden Arrays ARR_PERCENTAGES und ARR_PID
+	#Dies ist das Herzstück des Scripts, und zwar geht es hier um die Überprüfung der ganzen Zahlen aus den beiden Arrays ARR_PERCENTAGES und ARR_PID
 	#Mit einer For-Schleife loope ich durch jede PID im Array ARR_PID
-	#In der if Abfrage habe ich den Array ARR_PID_CHECK verbaut um wiederholungen zu vermeiden
-	#Beim 'ps aux | fgrep ..' wird die entsprechende PID gesucht und danach in die Email eingefügt, '#BUG#:' hier kann es jedoch zur Redundanz in der Email kommen, weil die PID zufällig in der Spalte COMMAND als Dateiname oder Befehl vorkommen kann. Dies hat aber keinerlei auswirkungen auf den kill Befehl, er wird lediglich nur der Prozess gekillt der am Meisten %CPU verbraucht
+	#In der if Abfrage habe ich den Array ARR_PID_CHECK verbaut, um Wiederholungen zu vermeiden
+	#Beim 'ps aux | fgrep ..' wird die entsprechende PID gesucht und danach in die Email eingefügt, '#BUG#:' hier kann es jedoch zur Redundanz in der Email kommen, weil die PID zufällig in der Spalte COMMAND als Dateiname oder Befehl vorkommen kann. Dies hat aber keinerlei auswirkungen auf den kill Befehl, er wird lediglich nur der Prozess gekillt, der am Meisten %CPU verbraucht
 	#Danach wird der Pfad der Datei email.[PID] an den emailer.sh mit dem Betreff übergeben
 	#Die Email wird gelöscht und der email_check.sh mit dem Parameter PID im Hintergrung gestertet
 
@@ -69,7 +69,7 @@ do
 			echo "$PS_HEADER" >> $EMAIL	#Erstelle eine Datei mit dem Header namens email.[PID]
 			echo >> $EMAIL
 
-			ps aux | fgrep "${ARR_PID[$i]}" | fgrep -v grep >> $EMAIL	#Grep die ganze Zeile von dem Bestimmten Prozess ID
+			ps aux | fgrep "${ARR_PID[$i]}" | fgrep -v grep >> $EMAIL	#Grep die ganze Zeile von dem bestimmten Prozess ID
 			ARR_PID_CHECK+=("${ARR_PID[$i]}")				#Füge die Prozess ID in den Array hinzu
 
 			echo >> $EMAIL
@@ -80,7 +80,7 @@ do
 
 			rm -f $EMAIL						#Lösche die Datei email.[PID]
 
-			$EMAIL_LISTENER "${ARR_PID[$i]}" &			#Starte den Eamil listener
+			$EMAIL_LISTENER "${ARR_PID[$i]}" &			#Starte den Email listener
 
 		fi
 	done
